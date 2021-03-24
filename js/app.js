5aa25df01fed5ddf23a5f2128259b8617b5db5a5
vupa2 <anhvu.pa96@gmail.com>
2021-03-24 18:07:27 +0700

    backup

--- js/app.js
+++ js/app.js
@@ -1,28 +1,47 @@
-const canvas = document.getElementById("mainGame"), ctx = canvas.getContext("2d");
+const canvas = document.getElementById("main-game"), ctx = canvas.getContext("2d");
 
-// Functions chung
-const getRandomColor = () => {
-  function getRandomHex() {
-    return Math.floor(Math.random() * 255);
-  }
-  return `rgb(${getRandomHex()}, ${getRandomHex()}, ${getRandomHex(), 1})`;
+// Tham so
+const LIFE = 3;
+
+const BALL_RADIUS = 8,
+      BALL_X = canvas.width / 2,
+      BALL_Y = canvas.height / 1.4,
+      BALL_SPEED = 4,
+      BALL_COLOR = '#e7e6e1';
+
+const PADDLE_WIDTH = 80,
+      PADDLE_HEIGHT = 8,
+      PADDLE_X = (canvas.width - PADDLE_WIDTH) / 2,
+      PADDLE_Y = canvas.height - PADDLE_HEIGHT - 20,
+      PADDLE_COLOR = '#f2a154',
+      PADDLE_SPEED = 12;
+
+const BRICK_WIDTH = 67,
+      BRICK_HEIGHT = 27,
+      BRICK_COLOR = '#e84545';
+
+const PupType = {
+  INCREASE: {color: "#17FF00", symbol: "I"},
+  DECRESE: {color: "#FFD500", symbol: "D"},
+  LIFE: {color: "#f875aa", symbol: "L"}
 }
 
 // Doi Tuong
 const ball = new Ball();
 const paddle = new Paddle();
 const brickWall = new BricksWall();
+const game = new Game();
 brickWall.setUp()
 
 const drawAll = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#2b2e4a";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
+
   brickWall.draw();
   paddle.draw();
   ball.draw();
-  update(ball, paddle, brickWall);
+  update(game, ball, paddle, brickWall);
 
   requestAnimationFrame(drawAll);
 }
--- js/function.js
+++ js/function.js
@@ -1,7 +1,15 @@
-const update = (ball, paddle, brickWall) => {
+const randomNumber = (max, min = 0) => {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+const getRandomColor = () => {
+  return `rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255), 1})`;
+}
+
+const update = (game, ball, paddle, brickWall) => {
   // Thay doi chieu speed khi cham tuong
-  if (ball.x + ball.dx > canvas.width - ball.ballRadius || ball.x + ball.dx < ball.ballRadius) {
-    ball.dx = -ball.dx;
+  if (ball.x + ball.dx * 1.01 > canvas.width - ball.ballRadius || ball.x + ball.dx < ball.ballRadius) {
+    ball.dx = -ball.dx * 1.01;
   }
 
   // Thay doi chieu speed khi cham tuong + paddle
@@ -11,32 +19,61 @@ const update = (ball, paddle, brickWall) => {
     ball.dy = -ball.dy;
   } else if (ball.y + ball.dy > canvas.height - ball.ballRadius) {
     ball.dy = -ball.dy; // lose
+    game.decreaseLife();
     // alert("GAME OVER");
     // document.location.reload();
   }
 
+  // Di chuyen Paddle
+  if (rightPressed) {
+    paddle.x += paddle.speed;
+    if (paddle.x + paddle.width > canvas.width) paddle.x = canvas.width - paddle.width;
+  } else if (leftPressed) {
+    paddle.x -= paddle.speed;
+    if (paddle.x < 0) paddle.x = 0;
+  }
+
   // Thay doi chieu speed khi cham brick
-  for (let col = 0; col < brickWall.brickColumnCount; col++) {
-    for (let row = 0; row < brickWall.brickRowCount; row++) {
-      const bricks = brickWall.bricks[col][row];
-      const powerUps = brickWall.powerUps[col][row];
+  if (brickWall.bricks.every(col => col.every(brick => brick.status === false))) {
+    brickWall.setUp();
+  } else {
+    for (let col = 0; col < brickWall.brickColumnCount; col++) {
+      for (let row = 0; row < brickWall.brickRowCount; row++) {
+        const bricks = brickWall.bricks[col][row];
+        const powerUps = brickWall.powerUps[col][row] ?? null;
 
-      if (bricks.status) {
-        if (ball.x >= bricks.x && ball.x <= bricks.x + bricks.width && ball.y >= bricks.y && ball.y <= bricks.y + bricks.height) {
-          ball.dy = -ball.dy * 1.005;
+        if (bricks.status) {
+          if (ball.x >= bricks.x && ball.x <= bricks.x + bricks.width && ball.y >= bricks.y && ball.y <= bricks.y + bricks.height) {
+            game.increaseScore();
 
-          bricks.status = false;
-          powerUps.status = true;
+            ball.dy = -ball.dy * 1.01;
+
+            bricks.status = false;
+            if (powerUps) powerUps.status = true;
+          }
         }
-      }
 
-      if (powerUps.status) {
-        if (powerUps.y + powerUps.height > paddle.y && powerUps.x > paddle.x && powerUps.x + powerUps.width < paddle.x + paddle.width) {
-          powerUps.status = false;
-        } else if (powerUps.y + powerUps.height > canvas.height) {
-          powerUps.status = false;
-        } else {
-          powerUps.y += powerUps.speed;
+        if (powerUps && powerUps.status) {
+          if (powerUps.y + powerUps.height > paddle.y && powerUps.x > paddle.x && powerUps.x + powerUps.width < paddle.x + paddle.width) {
+            powerUps.status = false;
+
+            switch (powerUps.symbol) {
+              case 'I':
+                paddle.width = paddle.width + 50 > canvas.width ? canvas.width : paddle.width + 50;
+                break;
+              case 'D':
+                paddle.width = paddle.width - 50 < PADDLE_WIDTH - 20 ? PADDLE_WIDTH - 20 : paddle.width - 50;
+                break;
+              case 'L':
+                game.increaseLife();
+                break;
+            }
+
+          } else if (powerUps.y + powerUps.height > canvas.height) {
+            powerUps.status = false;
+          } else {
+            powerUps.y += powerUps.speed;
+          }
         }
       }
     }
